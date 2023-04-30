86ca96fac91c56643c185247d7645efaacea08e9
marki259 <marcandrechenier@gmail.com>
2023-04-30 13:54:57 +0200

    Bunch of ownership corrections

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,8 @@ fn aitchison_aitken_kernel(h: f64, x_input: ArrayView1<f64>, x_new: f64) -> Arra
     kernel_values
 }
 
-fn gpke(bw: Vec<f64>, data: Array2<f64>, data_predict: Array1<f64>, var_type: Vec<&str>) -> Array1<f64> {
-    let mut kval = data.clone();
+fn gpke(bw: &Vec<f64>, data: ArrayView2<f64>, data_predict: ArrayView1<f64>, var_type: Vec<&str>) -> Array1<f64> {
+    let mut kval = data.to_owned().clone();
     for (i, vtype) in var_type.into_iter().enumerate() {
         let data_arr = kval.slice(s![.., i]);
         let data_new = data_predict.slice(s![i]).into_scalar();
@@ -51,13 +51,24 @@ fn gpke(bw: Vec<f64>, data: Array2<f64>, data_predict: Array1<f64>, var_type: Ve
     kval.fold_axis(Axis(1), 1.0, |acc, &x| acc * x)
 }
 
-fn est_loc_constant(bw: Vec<f64>, data_endog: Array1<f64>, data_exog: Array2<f64>, data_predict: Array1<f64>, var_type: Vec<&str>) -> f64 {
+fn est_loc_constant(bw: &Vec<f64>, data_endog: ArrayView1<f64>, data_exog: ArrayView2<f64>, data_predict: ArrayView1<f64>, var_type: Vec<&str>) -> f64 {
     let kernel_products = gpke(bw, data_exog, data_predict, var_type);
-    let loc_num = (data_endog * kernel_products.view()).sum();
+    let loc_num = (data_endog.to_owned() * kernel_products.view()).sum();
     let loc_denom = kernel_products.sum();
     loc_num/loc_denom
 }
 
+fn loc_constant_fit(bw: Vec<f64>, data_endog: ArrayView1<f64>, data_exog: ArrayView2<f64>, data_predict: Array2<f64>, var_type: Vec<&str>) -> Vec<f64> {
+    let n_predict = data_predict.len_of(Axis(0));
+    let mut local_means: Vec<f64> = Vec::with_capacity(n_predict);
+
+    for i in 0..n_predict {
+        let slice_predict = data_predict.slice(s![i, ..]);
+        local_means[i] = est_loc_constant(&bw, data_endog, data_exog, slice_predict, var_type.clone());
+    }
+
+    local_means
+}
 
 fn main() {
     let bw = vec![1.0, 0.2];
@@ -71,7 +82,7 @@ fn main() {
 
     // let output = gpke(bw, x_train, x_new, var_type);
 
-    let output = est_loc_constant(bw, y_train, x_train, x_new, var_type);
+    let output = est_loc_constant(&bw, y_train.view(), x_train.view(), x_new.view(), var_type);
 
     println!("{:#?}", output)
 }
