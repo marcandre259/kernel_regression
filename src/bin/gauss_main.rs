49c1436f49bb33aa816f2379921a00e3605ddbb6
marc <marcandrechenier@gmail.com>
2023-05-07 21:59:19 +0200

    Some fun with gaussian sigmoid

--- src/bin/gauss_main.rs
+++ src/bin/gauss_main.rs
@@ -1,28 +1,26 @@
-use ndarray::{Array, Array1};
+use ndarray::{Array, Array1, Array2};
 
 fn main() {
-    let x_values = Array::linspace(-4.0, 4.0, 10);
-    let y_values = Array::linspace(-3.0, 2.0, 10);
+    let x_values = Array::linspace(-0.20, 0.20, 3);
+    let y_values = Array::linspace(-3.0, 2.0, 3);
 
-    let kernel = exponentiated_quadratic(&Scalarray::Multiple(x_values, y_values));
+    let sigma: Array2<f64> = Array1::<f64>::exponentiated_quadratic(&x_values, &x_values);
 
-    println!("{:?}", kernel);
+    println!("{:.5?}", sigma);
 }
 
-enum Scalarray {
-    Single(f64, f64),
-    Multiple(Array1<f64>, Array1<f64>),
+trait Kernel<T, D> {
+    fn exponentiated_quadratic(x: T, y: T) -> D;
 }
 
-fn exponentiated_quadratic(x: &Scalarray) -> f64 {
-    let mut nugget: f64;
-    match x {
-        Scalarray::Single(xa, xb) => {
-            nugget = -0.5 * (xa - xb).powf(2.0);
+impl Kernel<&Array1<f64>, Array2<f64>> for Array1<f64> {
+    fn exponentiated_quadratic(x: &Array1<f64>, y: &Array1<f64>) -> Array2<f64> {
+        let mut outer: Array2<f64> = Array2::zeros((y.len(), x.len()));
+        for (i, a) in x.iter().enumerate() {
+            for (j, b) in y.iter().enumerate() {
+                outer[(j, i)] = (-0.5 * (a - b).powf(2.0)).exp();
+            }
         }
-        Scalarray::Multiple(xa, xb) => {
-            nugget = -0.5 * (xa - xb).dot(&(xa - xb));
-        }
+        outer
     }
-    nugget.exp()
 }
