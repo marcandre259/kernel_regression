576fc2fc4177befee0ed72c6adfe4e1494853186
marc <marcandrechenier@gmail.com>
2023-05-08 19:13:47 +0200

    Sampling from a multivariate...

--- src/bin/gauss_main.rs
+++ src/bin/gauss_main.rs
@@ -1,10 +1,18 @@
 use ndarray::{Array, Array1, Array2};
+use ndarray_linalg::cholesky::*;
+use ndarray_rand::rand::SeedableRng;
+use ndarray_rand::{rand_distr::Normal, RandomExt};
+use rand_chacha::ChaCha20Rng;
+
+const N: usize = 40;
 
 fn main() {
-    let x_values = Array::linspace(-0.20, 0.20, 3);
-    let y_values = Array::linspace(-3.0, 2.0, 3);
+    let x_values = Array::linspace(-2.0, 2.0, N);
 
     let sigma: Array2<f64> = Array1::<f64>::exponentiated_quadratic(&x_values, &x_values);
+    let mn: Array1<f64> = Array1::<f64>::zeros(N);
+
+    MultiNormal::new();
 
     println!("{:.5?}", sigma);
 }
@@ -22,5 +30,26 @@ impl Kernel<&Array1<f64>, Array2<f64>> for Array1<f64> {
             }
         }
         outer
+    }
+}
+
+struct MultiNormal {
+    mn: Array1<f64>,
+    sigma: Array2<f64>,
+}
+
+impl MultiNormal {
+    fn new(mn: Array1<f64>, sigma: Array2<f64>) {}
+
+    fn sample<E>(&self, n: usize) -> Array2<f64> {
+        let mut rng = ChaCha20Rng::seed_from_u64(25);
+
+        let normal = Normal::new(0.0, 1.0).unwrap();
+
+        let samples = Array::random_using((n, n), normal, &mut rng);
+
+        let chol = &self.sigma.cholesky(UPLO::Lower).unwrap();
+
+        &chol.dot(&samples).t() + &self.mn.broadcast(samples.dim()).unwrap()
     }
 }
